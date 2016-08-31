047dd6db030fd51010e4ae2a163030a5c00476ce
Bryan Baker <bryanpbaker@gmail.com>
2016-08-31 16:24:24 -0500

    update server.js

--- server.js
+++ server.js
@@ -2,19 +2,10 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var middleware = require('./middleware.js');
-
-
-
-app.use(middleware.logger);
-
-app.get('/about', middleware.requireAuthentication, function(req, res) {
-	res.send('About Us');
+app.get('/', function(req, res) {
+	res.send('ToDo API Root');
 });
 
-app.use(express.static(__dirname + '/public'));
-
-
 app.listen(PORT, function() {
-	console.log('Express server started on port ' + PORT);
+	console.log('Express listening on port ' + PORT + '!');
 });
