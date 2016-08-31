e92e0ce36747db6d6a39c3c576ef5594da74943e
Bryan Baker <bryanpbaker@gmail.com>
2016-08-31 15:04:37 -0500

    update server.js

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 app.use(express.static(__dirname + '/public'));
 
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
 	console.log('Express server started on port ' + PORT);
 });
