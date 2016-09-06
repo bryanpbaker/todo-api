1b3d405b396a5833a78e0d26bb78635a555f21ae
Bryan Baker <bryanpbaker@gmail.com>
2016-09-06 11:20:15 -0500

    refactor with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,13 +21,7 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.map(function(todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if(matchedTodo) {
 		res.json(matchedTodo);
@@ -37,8 +32,16 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed', 'id');
+	
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
 
+	// trim body.description
+	body.description = body.description.trim();
+
+	// add id field
 	body.id = todoNextId++;
 
 	todos.push(body);
