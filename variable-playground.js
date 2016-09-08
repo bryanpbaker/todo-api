a1f31fb131fab5de7c0aae9a4a4048b4b3b39b7b
Bryan Baker <bryanpbaker@gmail.com>
2016-09-08 10:33:32 -0500

    added variable-playground.js

---
+++ variable-playground.js
@@ -0,0 +1,18 @@
+// var person = {
+// 	name: 'Bryan',
+// 	age: 31
+// }
+
+// function updatePerson(obj) {
+
+// }
+
+var grades = [79, 85];
+
+function addGrades(gradesArr) {
+	gradesArr = [12, 33, 99]
+	debugger;
+}	
+
+addGrades(grades);
+console.log(grades);
