1304d569f42b3c3a1881867aaa6374d8f29ba47b
Bryan Baker <bryanpbaker@gmail.com>
2016-11-10 08:44:25 -0600

    enabled cors

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json(), function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.get('/', function(req, res) {
 	res.send('ToDo API Root');
